151d63f1a1667d4430dd9b697ebb1e24da8c69bc
Dominik Berner <dominik.berner@bbv.ch>
2021-10-08 09:56:01 +0000

    Fix formatting and cmake-format configuration

--- cmake-format.py
+++ cmake-format.py
@@ -25,15 +25,15 @@ with section("format"):
 
    # If an argument group contains more than this many sub-groups (parg or kwarg
   # groups) then force it to a vertical layout.
-  max_subgroups_hwrap = 2
+  max_subgroups_hwrap = 1
 
   # If a positional argument group contains more than this many arguments, then
   # force it to a vertical layout.
-  max_pargs_hwrap = 4
+  max_pargs_hwrap = 2
 
   # If a cmdline positional group consumes more than this many lines without
   # nesting, then invalidate the layout (and nest)
-  max_rows_cmdline = 2
+  max_rows_cmdline = 1
 
   # If the trailing parenthesis must be 'dangled' on its on line, then align it
   # to this reference: `prefix`: the start of the statement,  `prefix-indent`:
