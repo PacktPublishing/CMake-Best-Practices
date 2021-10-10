a61e57fdb5d74647c13965e456e35b143f28983f
Mustafa Kemal GILOR <mustafagilor@gmail.com>
2021-10-10 13:53:05 +0300

    Added cpack example

---
+++ chapter_4/ex06_pack/library/include/chapter4/ex06/lib.hpp
@@ -0,0 +1,24 @@
+/**
+ * ______________________________________________________
+ * Abstrace component interface description
+ *
+ * SPDX-License-Identifier:	MIT
+ * ______________________________________________________
+ */
+
+#pragma once
+
+namespace chapter4 {
+namespace ex06 {
+/**
+ * @brief The 'greeter' class interface
+ */
+class greeter {
+public:
+  /**
+   * Greet the caller.
+   */
+  void greet();
+}; // class greeter
+} // namespace ex06
+} // namespace chapter4
