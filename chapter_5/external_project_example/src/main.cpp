ceb7d86d587a14c5044727721728b248407eca1f
Dominik Berner <dominik.berner@gmail.com>
2021-10-30 11:54:35 +0000

    Add external project example

---
+++ chapter_5/external_project_example/src/main.cpp
@@ -0,0 +1,10 @@
+#include <bertrand/bertrand.hpp>
+#include <iostream>
+
+int main(int argc, char**)
+{
+    Require(argc > 0, "Argc is bigger than 0");
+
+    std::cout << "Welcome to the fetch_content example\n";
+    
+}
