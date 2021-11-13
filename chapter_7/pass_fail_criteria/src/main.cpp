1534dadd9cf73b17a0cd76232a6e506a9764d249
Dominik Berner <dominik.berner@bbv.ch>
2021-11-13 21:16:12 +0000

    Add example for pass and fail criteria

---
+++ chapter_7/pass_fail_criteria/src/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main(int argc, char **) {
+
+  if (argc == 1) {
+    std::cout << "Error: No arguments passed\n";
+  } else if (argc % 2 == 0) {
+    std::cout << "Warning: Even number of arguments" << std::endl;
+  } else {
+    std::cout << "Success: Odd number of arguments" << std::endl;
+  }
+
+  return argc % 2;
+}
