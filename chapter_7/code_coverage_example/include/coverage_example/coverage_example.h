b78684bfafaaf4abd92e6a8c90fad50d776680c4
Dominik Berner <dominik.berner@bbv.ch>
2021-11-16 05:57:20 +0000

    Add project stub for code coverage

---
+++ chapter_7/code_coverage_example/include/coverage_example/coverage_example.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <vector>
+
+namespace coverage_example
+{
+    /// this function checks if a number is dividable by the prime numbers between
+    /// 1 and 10 and returns the prime numbers as vcector
+    std::vector<int> check_for_divisibilty(int i);
+    
+} // namespace coverage_example
