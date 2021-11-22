5b5ee5502957558c5648fb90852a936e5bf2f09b
Dominik Berner <dominik.berner@bbv.ch>
2021-11-22 22:07:00 +0000

    Add example for test discovery

---
+++ chapter_7/test_discovery_example/src/test.cpp
@@ -0,0 +1,12 @@
+#define CATCH_CONFIG_MAIN /* This tells Catch to provide a main() - only do */
+
+#include <catch2/catch.hpp>
+
+std::uint64_t Fibonacci(std::uint64_t number) {
+  return number < 2 ? 1 : Fibonacci(number - 1) + Fibonacci(number - 2);
+}
+
+TEST_CASE("Fibonacci(0) returns 1") { REQUIRE(Fibonacci(0) == 1); }
+TEST_CASE("Fibonacci(1) returns 1") { REQUIRE(Fibonacci(1) == 1); }
+TEST_CASE("Fibonacci(2) returns 1") { REQUIRE(Fibonacci(2) == 2); }
+TEST_CASE("Fibonacci(5) returns 8") { REQUIRE(Fibonacci(5) == 8); }
