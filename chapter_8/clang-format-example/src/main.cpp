771a0cf072dd229a3f3158e2d9fd940310fe952d
Dominik Berner <dominik.berner@gmail.com>
2021-12-02 21:25:04 +0000

    Add example which generates a file

--- chapter_8/clang-format-example/src/main.cpp
+++ chapter_8/clang-format-example/src/main.cpp
@@ -1,7 +1,8 @@
-    #include <iostream>
+#include <iostream>
 
-/// this file is intentionally formatted badly to show how clang-format can correct this
+/// this file is intentionally formatted badly to show how clang-format can
+/// correct this
 int main() {
-        std::cout << "Welcome to a the clang-format example!" << std::endl;
+  std::cout << "Welcome to a the clang-format example!" << std::endl;
   return 0;
 }
