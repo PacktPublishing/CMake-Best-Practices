bcd8a05ccb5cb27505bc32840bb570cb54647d1e
Dominik Berner <dominik.berner@bbv.ch>
2021-12-22 08:05:33 +0100

    Using different libraries for external project and fetchcontent

--- chapter_5/fetch_content_example/src/main.cpp
+++ chapter_5/fetch_content_example/src/main.cpp
@@ -1,10 +1,15 @@
-#include <bertrand/bertrand.hpp>
+#include <SI/velocity.h>
+#include <SI/stream.h>
+
 #include <iostream>
+
+using namespace SI::literals;
 
 int main(int argc, char**)
 {
-    Require(argc > 0, "Argc is bigger than 0");
+    constexpr auto speed_of_a_swallow_in_m = 11.2_m_p_s;
+    constexpr SI::kilometre_per_hour_t<long double> speed_in_km = speed_of_a_swallow_in_m;
 
-    std::cout << "Welcome to the fetch_content example\n";
+    std::cout << "Did you know that an unladen swallow travels at approximately " << speed_of_a_swallow_in_m << " which is " << speed_in_km << "\n";
     
 }
