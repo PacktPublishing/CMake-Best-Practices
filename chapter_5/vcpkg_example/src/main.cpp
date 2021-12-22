69b71d5348fc83b903f2e1c80b25a46d17b75312
Dominik Berner <dominik.berner@bbv.ch>
2021-12-22 21:42:14 +0000

    Add vcpkg example

---
+++ chapter_5/vcpkg_example/src/main.cpp
@@ -0,0 +1,15 @@
+#include <rapidjson/document.h>
+
+int main(int, char**) {
+
+  const char json[] = " { \"hello\" : \"world\", \"t\" : true , \"f\" : false, \"n\": null, \"i\":123, \"pi\": 3.1416, \"a\":[1, 2, 3, 4] } ";
+  
+  rapidjson::Document document; 
+  document.parse(json);
+
+  if(document.HasMember("hello"))
+  {
+    std::cout << "Hello = " << document["hello"].GetString() << "\n";
+  }
+  
+}
