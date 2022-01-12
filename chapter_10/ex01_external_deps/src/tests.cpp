7521283d0f4351e0f7d22d7e938b2f22ad020158
Mustafa Kemal GILOR <mustafagilor@gmail.com>
2022-01-12 18:29:01 +0300

    Chapter 10: Initial example development

---
+++ chapter_10/ex01_external_deps/src/tests.cpp
@@ -0,0 +1,15 @@
+/**
+ * ______________________________________________________
+ * @file tests.cpp
+ *
+ * Simple unit tests
+ *
+ * SPDX-License-Identifier:	MIT
+ * ______________________________________________________
+ */
+
+#include <gtest/gtest.h>
+
+TEST(simple, test){
+    SUCCEED();
+}
