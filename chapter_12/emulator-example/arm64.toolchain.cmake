12012cbe1f2d232c9d31094aa0347119303ec241
Dominik Berner <dominik.berner@bbv.ch>
2022-02-02 15:15:02 +0000

    Add example for emulator

---
+++ chapter_12/emulator-example/arm64.toolchain.cmake
@@ -0,0 +1,7 @@
+set(CMAKE_SYSTEM_NAME Linux)
+
+# set(CMAKE_SYSROOT /home/doeme/tmp/sysroot/raspi/sysroot/)
+# set(CMAKE_STAGING_PREFIX /home/doeme/tmp/sysroot/raspi/staging)
+
+set(CMAKE_C_COMPILER arm-linux-gnueabi-gcc-9)
+set(CMAKE_CXX_COMPILER /usr/bin/arm-linux-gnueabihf-g++-9)
