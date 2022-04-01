6b67a25b891a5ebb0a030cc9b918efd4cd7dc4ac
Dominik Berner <dominik.berner@gmail.com>
2022-04-01 17:15:25 +0000

    Add example for resource groups

---
+++ chapter_7/resource_group_example/src/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <thread>
+#include <chrono>
+
+int main(int argc, char **) {
+
+using namespace std::literals::chrono_literals;
+std::this_thread::sleep_for(0.2s);
+
+return 0;
+
+}
