b9ed3467647124801f2812a24ea92fdded0f2dbd
Dominik Berner <dominik.berner@gmail.com>
2022-04-02 21:11:46 +0000

    Remove duplicate example

--- chapter_8/create_hash_example/src/main.cpp
+++
@@ -1,7 +0,0 @@
-    #include <iostream>
-
-/// this file is intentionally formatted badly to show how clang-format can correct this
-int main() {
-        std::cout << "Welcome to a the clang-format example!" << std::endl;
-  return 0;
-}
