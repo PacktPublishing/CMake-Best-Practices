5a7ae7adaf14ef78c575d6315b62b6ad127cc54f
Dominik Berner <dominik.berner@gmail.com>
2022-04-04 08:50:50 +0000

    Creating hash using custom command

--- chapter_8/chaining_custom_commands_example/src/main.cpp
+++ chapter_8/chaining_custom_commands_example/src/main.cpp
@@ -1,7 +1,6 @@
-    #include <iostream>
+#include <iostream>
 
-/// this file is intentionally formatted badly to show how clang-format can correct this
 int main() {
-        std::cout << "Welcome to a the clang-format example!" << std::endl;
+  std::cout << "Welcome to a the clang-format example!" << std::endl;
   return 0;
 }
