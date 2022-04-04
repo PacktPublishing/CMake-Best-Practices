efd9c6d18415354fc20c211a8b10b8345947f0c7
Dominik Berner <dominik.berner@gmail.com>
2022-04-04 21:54:19 +0000

    Add arm and emulator example to ci pipeline

--- chapter_12/emulator-example/arm64.toolchain.cmake
+++ chapter_12/emulator-example/arm64.toolchain.cmake
@@ -1,10 +1,6 @@
 set(CMAKE_SYSTEM_NAME Linux)
 set(CMAKE_SYSTEM_PROCESSOR arm)
 
-set(CMAKE_SYSROOT /usr/arm-linux-gnueabi)
-
-
-set(CMAKE_C_COMPILER /usr/bin/arm-linux-gnueabihf-gcc)
 set(CMAKE_CXX_COMPILER /usr/bin/arm-linux-gnueabihf-g++)
 
-set(CMAKE_CROSSCOMPILING_EMULATOR "qemu-arm;-L;${CMAKE_SYSROOT}") 
+set(CMAKE_CROSSCOMPILING_EMULATOR "qemu-arm;-L;/usr/arm-linux-gnueabihf") 
