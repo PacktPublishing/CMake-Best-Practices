7104a7727590ed72471aff878bb4053c9e1003f6
Dominik Berner <dominik.berner@gmail.com>
2022-04-04 08:50:50 +0000

    Creating hash using custom command

---
+++ chapter_8/custom_command_example/cmake/CreateSha256.cmake
@@ -0,0 +1,18 @@
+cmake_minimum_required(VERSION 3.21)
+
+
+if(CMAKE_ARGC LESS 5)
+    message(FATAL_ERROR "Usage: cmake -P CreateSha256.cmake file_to_hash target_file")
+endif()
+
+set(FILE_TO_HASH ${CMAKE_ARGV3})
+set(TARGET_FILE ${CMAKE_ARGV4})
+
+message(STATUS "Writing sha256 for file '${FILE_TO_HASH}' to file '${TARGET_FILE}'")
+
+# Read the source file and generate the hash for it
+file(SHA256 "${FILE_TO_HASH}" GENERATED_HASH)
+
+message(STATUS "sha256 for file '${FILE_TO_HASH}': ${GENERATED_HASH}")
+# write the hash to a new file
+file(WRITE "${TARGET_FILE}" "${GENERATED_HASH}")
