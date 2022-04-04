1ef1d2f71fe9b7dad7845e52c78667bb33b851ec
Dominik Berner <dominik.berner@gmail.com>
2022-04-04 21:36:08 +0000

    Squash me, edit emulator example

--- chapter_12/emulator-example/arm64.toolchain.cmake
+++ chapter_12/emulator-example/arm64.toolchain.cmake
@@ -1,7 +1,10 @@
 set(CMAKE_SYSTEM_NAME Linux)
+set(CMAKE_SYSTEM_PROCESSOR arm)
 
-# set(CMAKE_SYSROOT /home/doeme/tmp/sysroot/raspi/sysroot/)
-# set(CMAKE_STAGING_PREFIX /home/doeme/tmp/sysroot/raspi/staging)
+set(CMAKE_SYSROOT /usr/arm-linux-gnueabi)
 
-set(CMAKE_C_COMPILER arm-linux-gnueabi-gcc-9)
-set(CMAKE_CXX_COMPILER /usr/bin/arm-linux-gnueabihf-g++-9)
+
+set(CMAKE_C_COMPILER /usr/bin/arm-linux-gnueabihf-gcc)
+set(CMAKE_CXX_COMPILER /usr/bin/arm-linux-gnueabihf-g++)
+
+set(CMAKE_CROSSCOMPILING_EMULATOR "qemu-arm;-L;${CMAKE_SYSROOT}") 
